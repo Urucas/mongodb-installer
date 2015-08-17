cc47a47ddd5ec48f7b93648a280b98c5bc50a1ee
vrunoa <vruno.alassia@gmail.com>
2015-08-17 16:32:14 -0300

    update module paths

--- installer.js
+++ installer.js
@@ -1,12 +1,13 @@
 #!/usr/bin/env node
 var argv = require('minimist')(process.argv.slice(2))
-if(argv["help"]) {
+if(argv["help"] || !argv["dbpath"]) {
   console.log([
     "Usage; ",
     " mongodb-installer --dbpath <relative_path_to_db_folder>",
     ].join("\n"))
   process.exit(0)
 }
+
 var PlatformInstaller = require('./dist/PlatformInstaller.js'),
   installer = new PlatformInstaller(process.platform, argv["dbpath"]);
   
